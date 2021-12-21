e4f0b250e30f0661d966e6ae21c2d8d49fe53c2a
Isra08dc <90532141+Isra08dc@users.noreply.github.com>
2021-12-21 13:27:37 -0600

    Add files via upload

--- js/launcher.js
+++ js/launcher.js
@@ -1,4 +1,18 @@
+class launcher{
+	constructor(x,y)
 fly()
 {
     this.launcherObject.bodyA=null;
+}
+display()
+	{
+			var posBottom=this.bottomBody.position;
+			push()
+			translate(posBottom.x, posBottom.y+10);
+			fill(255)
+			imageMode(CENTER);
+			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
+			pop()
+			
+	}
 }
